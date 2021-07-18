697494eac2a5db64238c786d81d2dec11251f77e
JackWu <1121204968@qq.com>
2021-07-18 22:30:57 +0800

    iOS 代码完善

--- ios_calculator/calculator/Board.swift
+++ ios_calculator/calculator/Board.swift
@@ -23,14 +23,51 @@ class Board: UIView {
         installUI()
     }
     required init?(coder aDecoder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
+        super.init(coder: aDecoder)
+        installUI()
+
     }
     func installUI(){
         var frontBtn:FuncButton!
         for index in 0..<20{
             let btn = FuncButton()
             self.addSubview(btn)
-            
+            btn.snp.makeConstraints({(maker) in
+                if index%4 == 0{
+                    maker.left.equalTo(0)
+                }else{
+                    maker.left.equalTo(frontBtn.snp.right)
+                }
+                if index/4 == 0{
+                    maker.bottom.equalTo(0)
+                }else if index%4 == 0{
+                    maker.bottom.equalTo(frontBtn.snp.top)
+                }else{
+                    maker.bottom.equalTo(frontBtn.snp.bottom)
+                }
+                //等分 每一个btn占据父视图宽度的0.25
+                maker.width.equalTo(btn.superview!.snp.width).multipliedBy(0.25)
+                //等分 每一个btn占据父视图高度的0.2
+                maker.height.equalTo(btn.superview!.snp.height).multipliedBy(0.2)
+            })
+            btn.tag = index+100
+            btn.addTarget(self, action: #selector(btnClick(button:)), for: UIControl.Event.touchUpInside)
+            btn.setTitle(dataArray[index], for: UIControl.State.normal)
+            frontBtn = btn
         }
     }
+    @objc func btnClick(button:FuncButton)  {
+        print(button.title(for: .normal))
+    }
+}
+class Screen:UIView{
+    var inputLabel:UILabel?
+    var historyLabel:UILabel?
+    var inputString=""
+    var historyString=""
+    let figureArray:Array<Character>  = ["0","1","2","3","4","5","6","7","8","9","."]
+    
+
+
+
 }
--- ios_calculator/calculator/ViewController.swift
+++ ios_calculator/calculator/ViewController.swift
@@ -8,10 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let board = Board()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+      self.view.addSubview(board)
+        board.snp.makeConstraints{
+            (maker) in
+            maker.top.equalTo(100)
+            maker.height.equalTo(super.view!.snp.height).multipliedBy(0.7)
+            maker.width.equalTo(super.view!.snp.width)
+
+    }
     }
 
 
